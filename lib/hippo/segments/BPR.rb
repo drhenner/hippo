a5239e515f812fb4682aab20f996a6217420a5cc
Jonathan Jackson <jonj@promedicalinc.com>
2010-08-11 17:22:50 -0400

    Added the rest of the BPR fields, pg44

--- lib/hippo/segments/BPR.rb
+++ lib/hippo/segments/BPR.rb
@@ -16,5 +16,61 @@ module Hippo::Segments
     field :name     => 'PaymentMethodCode',
           :datatype => :list,
           :options  => ['ACH','BOP','CHK','FWT','NON']
+    
+    field :name     => 'PaymentFormatCode',
+          :datatype => :list,
+          :options  => ['CCP', 'CTX']
+
+    field :name     => 'IdNumberQualifier',
+          :datatype => :list,
+          :options  => ['01', '04']
+     
+    field :name     => 'DfiIdenficationNumber',
+          :datatype => :alpha_numeric
+
+    field :name     => 'AccountNumberQualifier',
+          :datatype => :alpha_numeric
+    
+    field :name     => 'AccountNumber',
+          :datatype => :alpha_numeric
+
+    field :name     => 'OriginatingCompanyIdentifier',
+          :datatype => :alpha_numeric
+    
+    field :name     => 'OriginatingCompanySupplementalCode',
+          :datatype => :alpha_numeric
+
+    field :name     => 'DfiIdNumberQualifier',
+          :datatype => :list,
+          :options  => ['01', '04']
+    
+    field :name     => 'DfiIdentificationNumber',
+          :datatype => :alpha_numeric
+    
+    field :name     => 'AccountNumberQualifier',
+          :datatype => :list,
+          :options  => ['DA', 'SG']
+    
+    field :name     => 'AccountNumber',
+          :datatype => :alpha_numeric
+
+    field :name     => 'Date',
+          :datatype => :date
+    
+    field :name     => 'BusinessFunctionCode',
+          :datatype => :alpha_numeric
+
+    field :name     => 'DfiIdNumberQualifier',
+          :datatype => :alpha_numeric
+
+    field :name     => 'DfiIdentificationNumber',
+          :datatype => :alpha_numeric
+
+    field :name     => 'AccountNumberQualifier',
+          :datatype => :alpha_numeric
+    
+    field :name     => 'AccountNumber',
+          :datatype => :alpha_numeric
   end
 end
+
