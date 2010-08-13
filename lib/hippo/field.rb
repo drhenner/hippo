4f21f67a0fa6dadaa5f52716ae03836f1581a3e5
Robert Jackson <robertj@promedicalinc.com>
2010-08-13 15:43:08 -0400

    Add composite procedure to SVC.

--- lib/hippo/field.rb
+++ lib/hippo/field.rb
@@ -1,6 +1,6 @@
 module Hippo
   class Field
-    attr_accessor :name, :sequence, :datatype, :options, :restrictions, :minimum, :maximum, :required
+    attr_accessor :name, :sequence, :datatype, :options, :restrictions, :minimum, :maximum, :required, :separator
 
   end
 end
--- lib/hippo/segments/SVC.rb
+++ lib/hippo/segments/SVC.rb
@@ -3,47 +3,133 @@ module Hippo::Segments
 
     segment_identifier  'SVC'
 
-    field   :name     => 'CompositeMedicalProcedureIdentifier1',
-            :datatype => :alpha_numeric,
-            :minimum  => 0,
-            :maximum  => inf,
-            :required => true
+    field   :name      => 'ProductServiceIDQualifier',
+            :datatype  => :alpha_numeric,
+            :minimum   => 2,
+            :maximum   => 2,
+            :required  => true,
+            :separator => Hippo::CompositeSeparator
+    
+    field   :name      => 'ProductServiceID',
+            :datatype  => :alpha_numeric,
+            :minimum   => 1,
+            :maximum   => 48,
+            :require   => true,
+            :separator => Hippo::CompositeSeparator
 
-    field   :name     => 'MonetaryAmount1',
-            :datatype => :numeric,
-            :minimum  => 1,
-            :maximum  => 18,
-            :required => true
+    field   :name      => 'ProcedureModifier1',
+            :datatype  => :alpha_numeric,
+            :minimum   => 2,
+            :maximum   => 2,
+            :require   => false,
+            :separator => Hippo::CompositeSeparator
 
-    field   :name     => 'MonetaryAmount2',
-            :datatype => :numeric,
-            :minimum  => 1,
-            :maximum  => 18,
-            :required => false
+    field   :name      => 'ProcedureModifier2',
+            :datatype  => :alpha_numeric,
+            :minimum   => 2,
+            :maximum   => 2,
+            :require   => false,
+            :separator => Hippo::CompositeSeparator
+    
+    field   :name      => 'ProcedureModifier3',
+            :datatype  => :alpha_numeric,
+            :minimum   => 2,
+            :maximum   => 2,
+            :require   => false,
+            :separator => Hippo::CompositeSeparator
+    
+    field   :name      => 'ProcedureModifier4',
+            :datatype  => :alpha_numeric,
+            :minimum   => 2,
+            :maximum   => 2,
+            :require   => false,
+            :separator => Hippo::CompositeSeparator
 
-    field   :name     => 'ProductServiceId',
-            :datatype => :alpha_numeric,
-            :minimum  => 1,
-            :maximum  => 48,
-            :required => false
+    field   :name      => 'Description',
+            :datatype  => :alpha_numeric,
+            :minimum   => 1,
+            :maximum   => 80,
+            :require   => false,
+            :separator => Hippo::CompositeSeparator
 
-    field   :name     => 'Quantity1',
-            :datatype => :numeric,
-            :minimum  => 1,
-            :maximum  => 15,
-            :required => false
+    field   :name      => 'MonetaryAmount1',
+            :datatype  => :numeric,
+            :minimum   => 1,
+            :maximum   => 18,
+            :required  => true
 
-    field   :name     => 'CompositeMedicalProcedureIdentifier2',
-            :datatype => :alpha_numeric,
-            :minimum  => 0,
-            :maximum  => inf,
-            :required => false
+    field   :name      => 'MonetaryAmount2',
+            :datatype  => :numeric,
+            :minimum   => 1,
+            :maximum   => 18,
+            :required  => false
 
-    field   :name     => 'Quantity2',
-            :datatype => :numeric,
-            :minimum  => 1,
-            :maximum  => 15,
-            :required => false
+    field   :name      => 'ProductServiceId',
+            :datatype  => :alpha_numeric,
+            :minimum   => 1,
+            :maximum   => 48,
+            :required  => false
+
+    field   :name      => 'Quantity1',
+            :datatype  => :numeric,
+            :minimum   => 1,
+            :maximum   => 15,
+            :required  => false
+
+    field   :name      => 'OriginalProductServiceIDQualifier',
+            :datatype  => :alpha_numeric,
+            :minimum   => 2,
+            :maximum   => 2,
+            :required  => false,
+            :separator => Hippo::CompositeSeparator
+    
+    field   :name      => 'OriginalProductServiceID',
+            :datatype  => :alpha_numeric,
+            :minimum   => 1,
+            :maximum   => 48,
+            :require   => false,
+            :separator => Hippo::CompositeSeparator
+
+    field   :name      => 'OriginalProcedureModifier1',
+            :datatype  => :alpha_numeric,
+            :minimum   => 2,
+            :maximum   => 2,
+            :require   => false,
+            :separator => Hippo::CompositeSeparator
+
+    field   :name      => 'OriginalProcedureModifier2',
+            :datatype  => :alpha_numeric,
+            :minimum   => 2,
+            :maximum   => 2,
+            :require   => false,
+            :separator => Hippo::CompositeSeparator
+    
+    field   :name      => 'OriginalProcedureModifier3',
+            :datatype  => :alpha_numeric,
+            :minimum   => 2,
+            :maximum   => 2,
+            :require   => false,
+            :separator => Hippo::CompositeSeparator
+    
+    field   :name      => 'OriginalProcedureModifier4',
+            :datatype  => :alpha_numeric,
+            :minimum   => 2,
+            :maximum   => 2,
+            :require   => false,
+            :separator => Hippo::CompositeSeparator
+
+    field   :name      => 'OriginalDescription',
+            :datatype  => :alpha_numeric,
+            :minimum   => 1,
+            :maximum   => 80,
+            :require   => false,
+            :separator => Hippo::CompositeSeparator
+
+    field   :name      => 'Quantity2',
+            :datatype  => :numeric,
+            :minimum   => 1,
+            :maximum   => 15,
+            :required  => false
 
   end
 end
