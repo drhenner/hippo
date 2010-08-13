b586bef685cddf1c83b19de1cb8855e29edaaeb6
Jonathan Jackson <jonj@promedicalinc.com>
2010-08-13 13:58:00 -0400

    Added Definition for the 2100 loop.

--- lib/hippo/transaction_sets/HIPAA_835.rb
+++ lib/hippo/transaction_sets/HIPAA_835.rb
@@ -55,18 +55,101 @@ module Hippo::TransactionSets
       segment Hippo::Segments::TS2
 
       loop    L2100, 
-              :idenitied_by => {'CLP.ClaimSubmittersIdentifier' => '!=nil'}
-              :minimum => 1
+              :idenitied_by => {'CLP.ClaimSubmittersIdentifier' => '!=nil'},
+              :minimum => 1,
               :maximum => nil
     end
 
+    
     class L2100 < Hippo::TransactionSets::Base
-      segment Hippo::Segments::CLP
-      segment Hippo::Segments::CAS,
+      segment Hippo::Segments::CLP, #Claim Payment Information
+              :minimum => 1,
+              :maximum => 1
+      segment Hippo::Segments::CAS, #Claim Adjustment
               :minimum => 0,
               :maximum => 99
       segment Hippo::Segments::NM1,
-              'EntityIdentifierCode' => 'QC'
+              'EntityIdentifierCode' => 'QC',  #Patient
+              :minimum => 1,
+              :maximum => 1
+      segment Hippo::Segments::NM1,
+              'EntityIdentifierCode' => 'IL',  #Insured or Subscriber
+              :minimum => 0,
+              :maximum  => 1
+      segment Hippo::Segments::NM1,
+              'EntityIdentifierCode' => '74',  #Corrected Insured
+              :minimum => 0,
+              :maximum => 1
+      segment Hippo::Segments::NM1,
+              'EntityIdentifierCode' => '82',  #Rendering Provider
+              :minimum => 0,
+              :maximum => 1
+     
+      segment Hippo::Segments::NM1,
+              'EntityIdentifierCode' => 'TT',  #Transfer To
+              :minimum => 0,
+              :maximum => 1
+      segment Hippo::Segments::NM1, 
+              'EntityIdentifierCode' => 'PR',  #Payer
+              :minimum => 0,
+              :maximum => 1
+      segment Hippo::Segments::MIA,
+              :minimum => 0,
+              :maximum => 1
+      segment Hippo::Segments::MOA,  
+              :minimum => 0,
+              :maximum => 1
+      segment Hippo::Segments::REF,  #Other Claim Related Identification
+              :minimum => 0,
+              :maximum => 99
+      segment Hippo::Segments::REF,  #Rendering Provider Identification
+              :minimum => 0,
+              :maximum => 10
+      segment Hippo::Segments::DTM,  #Claim Date
+              :minimum => 0,
+              :maximum => 4
+      segment Hippo::Segments::PER,  #Claim Contact Information
+              :minimum => 0,
+              :maximum => 3
+      segment Hippo::Segments::AMT,  #Claim Supplemental Information
+              :minimum => 0, 
+              :maximum => 14
+      segment Hippo::Segments::QTY, #Claim Supplemental Information Quantity
+              :minimum => 0,
+              :maximum => 15
+      segment Hippo::Segments::SVC, #Service Payment Information
+              :minimum => 0,
+              :maximum => 1
+      segment Hippo::Segments::DTM, #Service Date
+              :minimum => 0,
+              :maximum => 3
+      segment Hippo::Segments::CAS,  #Service Adjustment
+              :minimum => 0,
+              :maximum => 99
+      segment Hippo::Segments::REF,  #Service Identification
+              :minimum => 0,
+              :maximum => 7
+      segment Hippo::Segments::REF,  #Rendering Provider Information
+              :minimum => 0,
+              :maximum => 10
+      segment Hippo::Segments::AMT,  #Service Supplemental Amount
+              :minimum => 0,
+              :maximum => 12
+      segment Hippo::Segments::QTY,  #Service Supplemental Quantity
+              :minimum => 0,
+              :maximum => 6
+      segment Hippo::Segments::LQ,  #Health Care Remark Codes 
+              :minimum => 0,
+              :maximum => 99
+      segment Hippo::Segments::PLB,  #Provider Adjustment
+              :minimum => 1,
+              :maximum => nil
+      segment Hippo::Segments::SE,  #Transaction Set Trailer
+              :minimum => 1,
+              :maximum => 1
+
+
+              
 
     end
   end
