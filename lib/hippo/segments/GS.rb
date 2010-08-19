89863ecb9b39b8337f961925d21f29cee23d906e
Robert Jackson <robertj@promedicalinc.com>
2010-08-19 16:14:00 -0400

    Add ISA, GS, IEA, and GE to autoloads.

--- lib/hippo/segments/GS.rb
+++ lib/hippo/segments/GS.rb
@@ -5,7 +5,7 @@ module Hippo::Segments
 
     field   :name     => 'FunctionalIdentifierCode',
             :datatype => :list,
-            :list     => [ 'AA','AB','AC','AD','AE','AF','AG','AH','AI','AK','AL','AM','AN','AO','AP','AQ','AR','AS','AT','AU','AV','AW','AX','AY','AZ','BA','BB','BC','BD','BE','BF','BL','BS','CA','CB','CC','CD','CE','CF','CH','CI','CJ','CK','CL','CM','CN','CO','CP','CQ','CR','CS','CT','CU','CV','CW','D3','D4','D5','DA','DD','DF','DI','DM','DS','DX','EC','ED','EI','EP','ER','ES','EV','EX','FA','FB','FC','FG','FH','FR','FT','GB','GC','GE','GF','GL','GP','GR','GT','HB','HC','HI','HN','HP','HR','HS','HU','HV','IA','IB','IC','ID','IE','IF','IG','IH','IJ','IM','IN','IO','IR','IS','JB','KM','LA','LB','LI','LN','LR','LS','LT','MA','MC','MD','ME','MF','MG','MH','MI','MJ','MK','MM','MN','MO','MP','MQ','MR','MS','MT','MV','MW','MX','MY','MZ','NC','NL','NP','NR','NT','OC','OG','OR','OW','PA','PB','PC','PD','PE','PF','PG','PH','PI','PJ','PK','PL','PN','PO','PQ','PR','PS','PT','PU','PV','PW','PY','QG','QM','QO','RA','RB','RC','RD','RE','RF','RG','RH','RI','RJ','RK','RL','RM','RN','RO','RP','RQ','RR','RS','RT','RU','RV','RW','RX','RY','RZ','SA','SB','SC','SD','SE','SG','SH','SI','SJ','SL','SM','SN','SO','SP','SQ','SR','SS','ST','SU','SV','SW','TA','TB','TD','TE','TF','TI','TM','TN','TO','TP','TR','TS','TT','TU','TX','UA','UB','UC','UD','UI','UP','UW','VA','VB','VC','VD','VE','VH','VI','VS','WA','WB','WG','WI','WL','WR','WT']
+            :list     => [ 'AA','AB','AC','AD','AE','AF','AG','AH','AI','AK','AL','AM','AN','AO','AP','AQ','AR','AS','AT','AU','AV','AW','AX','AY','AZ','BA','BB','BC','BD','BE','BF','BL','BS','CA','CB','CC','CD','CE','CF','CH','CI','CJ','CK','CL','CM','CN','CO','CP','CQ','CR','CS','CT','CU','CV','CW','D3','D4','D5','DA','DD','DF','DI','DM','DS','DX','EC','ED','EI','EP','ER','ES','EV','EX','FA','FB','FC','FG','FH','FR','FT','GB','GC','GE','GF','GL','GP','GR','GT','HB','HC','HI','HN','HP','HR','HS','HU','HV','IA','IB','IC','ID','IE','IF','IG','IH','IJ','IM','IN','IO','IR','IS','JB','KM','LA','LB','LI','LN','LR','LS','LT','MA','MC','MD','ME','MF','MG','MH','MI','MJ','MK','MM','MN','MO','MP','MQ','MR','MS','MT','MV','MW','MX','MY','MZ','NC','NL','NP','NR','NT','OC','OG','OR','OW','PA','PB','PC','PD','PE','PF','PG','PH','PI','PJ','PK','PL','PN','PO','PQ','PR','PS','PT','PU','PV','PW','PY','QG','QM','QO','RA','RB','RC','RD','RE','RF','RG','RH','RI','RJ','RK','RL','RM','RN','RO','RP','RQ','RR','RS','RT','RU','RV','RW','RX','RY','RZ','SA','SB','SC','SD','SE','SG','SH','SI','SJ','SL','SM','SN','SO','SP','SQ','SR','SS','ST','SU','SV','SW','TA','TB','TD','TE','TF','TI','TM','TN','TO','TP','TR','TS','TT','TU','TX','UA','UB','UC','UD','UI','UP','UW','VA','VB','VC','VD','VE','VH','VI','VS','WA','WB','WG','WI','WL','WR','WT'],
             :required => false
 
     field   :name     => 'ApplicationSendersCode',
@@ -40,12 +40,12 @@ module Hippo::Segments
 
     field   :name     => 'ResponsibleAgencyCode',
             :datatype => :list,
-            :list     => [ 'T','X']
+            :list     => [ 'T','X'],
             :required => false
 
     field   :name     => 'VersionReleaseIndustryIdentifierCode',
             :datatype => :list,
-            :list     => [ '001000','002000','002001','002002','002003','002031','002040','002041','002042','003000','003010','003011','003012','003020','003021','003022','003030','003031','003032','003040','003041','003042','003050','003051','003052','003060','003061','003062','003070','003071','003072','004000','004010','004011','004012','004020','004021','004022','004030','004031','004032','004040','004041','004042','004050']
+            :list     => [ '001000','002000','002001','002002','002003','002031','002040','002041','002042','003000','003010','003011','003012','003020','003021','003022','003030','003031','003032','003040','003041','003042','003050','003051','003052','003060','003061','003062','003070','003071','003072','004000','004010','004011','004012','004020','004021','004022','004030','004031','004032','004040','004041','004042','004050'],
             :required => false
 
   end
--- lib/hippo/segments/ISA.rb
+++ lib/hippo/segments/ISA.rb
@@ -5,7 +5,7 @@ module Hippo::Segments
 
     field   :name     => 'AuthorizationInformationQualifier',
             :datatype => :list,
-            :list     => [ '00','01','02','03','04','05','06']
+            :list     => [ '00','01','02','03','04','05','06'],
             :required => false
 
     field   :name     => 'AuthorizationInformation',
@@ -16,7 +16,7 @@ module Hippo::Segments
 
     field   :name     => 'SecurityInformationQualifier',
             :datatype => :list,
-            :list     => [ '00','01']
+            :list     => [ '00','01'],
             :required => false
 
     field   :name     => 'SecurityInformation',
@@ -27,7 +27,7 @@ module Hippo::Segments
 
     field   :name     => 'InterchangeIdQualifier1',
             :datatype => :list,
-            :list     => [ '01','02','03','04','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','AM','NR','SN','ZZ']
+            :list     => [ '01','02','03','04','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','AM','NR','SN','ZZ'],
             :required => false
 
     field   :name     => 'InterchangeSenderId',
@@ -62,12 +62,12 @@ module Hippo::Segments
 
     field   :name     => 'InterchangeControlStandardsIdentifier',
             :datatype => :list,
-            :list     => [ 'U']
+            :list     => [ 'U'],
             :required => false
 
     field   :name     => 'InterchangeControlVersionNumber',
             :datatype => :list,
-            :list     => [ '00200','00201','00204','00300','00301','00302','00303','00304','00305','00306','00307','00400','00401','00402']
+            :list     => [ '00200','00201','00204','00300','00301','00302','00303','00304','00305','00306','00307','00400','00401','00402'],
             :required => false
 
     field   :name     => 'InterchangeControlNumber',
@@ -78,12 +78,12 @@ module Hippo::Segments
 
     field   :name     => 'AcknowledgmentRuested',
             :datatype => :list,
-            :list     => [ '0','1']
+            :list     => [ '0','1'],
             :required => false
 
     field   :name     => 'UsageIndicator',
             :datatype => :list,
-            :list     => [ 'I','P','T']
+            :list     => [ 'I','P','T'],
             :required => false
 
     field   :name     => 'ComponentElementSeparator',
