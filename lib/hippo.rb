676e8345fcde59ec4adffd342922114acde3c243
Robert Jackson <robertj@promedicalinc.com>
2010-11-26 12:34:27 -0500

    Remove Demo*** autoloads, and extra cruft.

--- lib/hippo.rb
+++ lib/hippo.rb
@@ -1,30 +1,14 @@
 libdir = File.dirname(__FILE__)
 $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 
-libdir = File.dirname(__FILE__)
-$LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
-
 module Hippo
   autoload :Segments,         'hippo/segments'
   autoload :TransactionSets,  'hippo/transaction_sets'
   autoload :Field,            'hippo/field'
   autoload :CompositeField,   'hippo/composite_field'
   autoload :Parser,           'hippo/parser'
-  autoload :Demo835,          '../bin/demo_835.rb'
-  autoload :Demo276,          '../bin/demo_276.rb'
   
   FieldSeparator = '*'
   CompositeSeparator = ':'
   SegmentSeparator = '~'
 end
-
-def random_chars(count = 5)
-  chars = ("a".."z").to_a + ("1".."9").to_a + ("A".."Z").to_a 
-  Array.new(count, '').collect{chars[rand(chars.size)]}.join
-end
-
-if __FILE__ == $0
-  #Hippo::Demo835.run
-  Hippo::Demo276.run
-end
-
