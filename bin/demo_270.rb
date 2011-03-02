515f947dc904b9e673d81cd0daef96257ba98dc7
Jonathan Jackson <jonj@promedicalinc.com>
2011-03-02 17:09:45 -0500

    Created Demo 270 to begin testing in symphonia.

--- bin/demo_270.rb
+++ bin/demo_270.rb
@@ -18,6 +18,7 @@ class Demo270
       seg.Time = ''
       seg.TransactionTypeCode = ''
     end
+
     ts.L2000A do |loop|
       loop.HL do |seg|
         seg.HierarchicalIdNumber = ''
@@ -33,6 +34,242 @@ class Demo270
           seg.NameFirst = ''
         end
       end
+    end
+    
+    ts.L2000B do |loop|
+      loop.HL do |seg|
+        seg.HierarchicalIdNumber = ''
+        seg.HierarchicalParentIdNumber = ''
+        seg.HierarchicalLevelCode = ''
+        seg.HierarchicalChildCode = ''
+      end
+      loop.L2100B do |sub_loop|
+        sub_loop.NM1 do |seg|
+          seg.EntityIdentifierCode1 = ''
+          seg.EntityTypeQualifier = ''
+          seg.NameLastOrOrganizationName = ''
+          seg.NameFirst = ''
+        end
+        sub_loop.REF do |seg|
+          seg.ReferenceIdentificationQualifier = ""
+          seg.ReferenceIdentification = ""
+          seg.Description = ""
+          seg.ReferenceIdentifier = ""
+        end
+        sub_loop.N3 do |seg|
+          seg.AddressInformation1 = ""
+          seg.AddressInformation2 = ""
+        end
+        sub_loop.N4 do |seg|
+					seg.CityName = ""
+					seg.StateOrProvinceCode = ""
+					seg.PostalCode = ""
+					seg.CountryCode = ""
+				end
+				sub_loop.PER do |seg|
+					seg.ContactFunctionCode = ""
+					seg.Name = ""
+				end
+				sub_loop.PRV do |seg|
+			    seg.ProviderCode = ""
+			    seg.ReferenceIdentificationQualifier = ""
+			    seg.ReferenceIdentification = ""
+			    seg.StateOrProvinceCode = ""
+			    seg.ProviderSpecialtyInformation = ""
+			    seg.ProviderOrganizationCode = ""
+			  end
+      end
+    end
+    
+    ts.L2000C do |loop|
+      loop.HL do |seg|
+        seg.HierarchicalIdNumber = ''
+        seg.HierarchicalParentIdNumber = ''
+        seg.HierarchicalLevelCode = ''
+        seg.HierarchicalChildCode = ''
+      end
+      loop.TRN do |seg|
+        seg.TraceTypeCode = rand(10 * 100)
+				seg.ReferenceIdentification1 = rand(10 * 100)
+				seg.OriginatingCompanyIdentifier = rand(10 * 100)
+				seg.ReferenceIdentification2 = rand(10 * 100)    
+      end
+      loop.L2100C do |sub_loop|
+        sub_loop.NM1 do |seg|
+          seg.EntityIdentifierCode1 = ''
+          seg.EntityTypeQualifier = ''
+          seg.NameLastOrOrganizationName = ''
+          seg.NameFirst = ''          
+        end
+        sub_loop.REF do |seg|
+          seg.ReferenceIdentificationQualifier = "00"
+          seg.ReferenceIdentification = "R0203"
+          seg.Description = "Description"
+          seg.ReferenceIdentifier = "1241"
+        end
+        sub_loop.N3 do |seg|
+          seg.AddressInformation1 = ""
+          seg.AddressInformation2 = ""
+        end
+        sub_loop.N4 do |seg|
+					seg.CityName = ""
+					seg.StateOrProvinceCode = ""
+					seg.PostalCode = ""
+					seg.CountryCode = ""
+				end
+				sub_loop.PRV do |seg|
+          seg.ProviderCode = ""
+          seg.ReferenceIdentificationQualifier = ""
+          seg.ReferenceIdentification = ""
+          seg.StateOrProvinceCode = ""
+          seg.ProviderSpecialtyInformation = ""
+          seg.ProviderOrganizationCode = ""			    
+			  end
+			  sub_loop.DMG do |seg|
+          seg.DateTimePeriodFormatQualifier = ''
+          seg.DateTimePeriod = ''
+          seg.GenderCode = ''
+          seg.MaritalStatusCode = ''
+          seg.CompositeRaceOrEthnicityInformation = ''
+          seg.CitizenshipStatusCode = ''
+          seg.CountryCode = ''
+          seg.BasisOfVerificationCode = ''
+          seg.Quantity = ''
+          seg.CodeListQualifierCode = ''
+          seg.IndustryCode = ''
+		    end
+		    sub_loop.INS do |seg|
+	        seg.YesNoConditionOrResponseCode = ''
+	        seg.IndividualRelationshipCode = ''
+	      end
+	      sub_loop.DTP do |seg|
+          seg.DateTimeQualifier = ''
+          seg.DateTimePeriodFormatQualifier = ''
+          seg.DateTimePeriod = ''
+        end
+        sub_loop.L2110C do |sub_loop_child|
+         sub_loop_child.EQ do |seg|
+          seg.ServiceTypeCode = ""
+          seg.CoverageLevelCode = ""
+          seg.InsuranceTypeCode = ""
+         end
+         sub_loop_child.AMT do |seg|
+           seg.AmountQualifierCode = ''
+           seg.MonetaryAmount = ''
+           seg.CreditDebitFlagCode = ''
+         end
+         sub_loop_child.III do |seg|
+          seg.CodeListQualifierCode = ""
+          seg.IndustryCode = ""
+         end
+         sub_loop_child.REF do |seg|
+           seg.ReferenceIdentificationQualifier = ""
+           seg.ReferenceIdentification = ""
+           seg.Description = ""
+           seg.ReferenceIdentifier = ""
+         end
+         sub_loop_child.DTP do |seg|
+           seg.DateTimeQualifier = ''
+           seg.DateTimePeriodFormatQualifier = ''
+           seg.DateTimePeriod = ''          
+         end
+        end
+      end
+    end  
+    
+    ts.L2000D do |loop|
+      loop.HL do |seg|
+        seg.HierarchicalIdNumber = ''
+        seg.HierarchicalParentIdNumber = ''
+        seg.HierarchicalLevelCode = ''
+        seg.HierarchicalChildCode = ''
+      end
+      loop.TRN do |seg|
+        seg.TraceTypeCode = rand(10 * 100)
+        seg.ReferenceIdentification1 = rand(10 * 100)
+        seg.OriginatingCompanyIdentifier = rand(10 * 100)
+        seg.ReferenceIdentification2 = rand(10 * 100)
+      end
+      loop.L2100D do |sub_loop|
+        sub_loop.NM1 do |seg|
+          seg.EntityIdentifierCode1 = ''
+          seg.EntityTypeQualifier = ''
+          seg.NameLastOrOrganizationName = ''
+          seg.NameFirst = ''
+        end
+        sub_loop.REF do |seg|
+          seg.ReferenceIdentificationQualifier = ""
+          seg.ReferenceIdentification = ""
+          seg.Description = ""
+          seg.ReferenceIdentifier = ""
+        end
+        sub_loop.N3 do |seg|
+          seg.AddressInformation1 = ""
+          seg.AddressInformation2 = ""
+        end
+        sub_loop.N4 do |seg|
+					seg.CityName = ""
+					seg.StateOrProvinceCode = ""
+					seg.PostalCode = ""
+					seg.CountryCode = ""
+				end
+				sub_loop.PRV do |seg|
+          seg.ProviderCode = ""
+          seg.ReferenceIdentificationQualifier = ""
+          seg.ReferenceIdentification = ""
+          seg.StateOrProvinceCode = ""
+          seg.ProviderSpecialtyInformation = ""
+          seg.ProviderOrganizationCode = ""			    
+			  end
+			  sub_loop.DMG do |seg|
+          seg.DateTimePeriodFormatQualifier = ''
+          seg.DateTimePeriod = ''
+          seg.GenderCode = ''
+          seg.MaritalStatusCode = ''
+          seg.CompositeRaceOrEthnicityInformation = ''
+          seg.CitizenshipStatusCode = ''
+          seg.CountryCode = ''
+          seg.BasisOfVerificationCode = ''
+          seg.Quantity = ''
+          seg.CodeListQualifierCode = ''
+          seg.IndustryCode = ''
+		    end
+		    sub_loop.INS do |seg|
+	        seg.YesNoConditionOrResponseCode = ''
+	        seg.IndividualRelationshipCode = ''
+	      end
+	      sub_loop.DTP do |seg|
+          seg.DateTimeQualifier = ''
+          seg.DateTimePeriodFormatQualifier = ''
+          seg.DateTimePeriod = ''
+        end
+        sub_loop.L2110D do |sub_loop_child|
+          sub_loop_child.EQ do |seg|
+            seg.ServiceTypeCode = ""
+            seg.CoverageLevelCode = ""
+            seg.InsuranceTypeCode = ""
+          end
+          sub_loop_child.III do |seg|
+             seg.CodeListQualifierCode = ""
+             seg.IndustryCode = ""
+          end
+          sub_loop_child.REF do |seg|
+            seg.ReferenceIdentificationQualifier = ""
+            seg.ReferenceIdentification = ""
+            seg.Description = ""
+            seg.ReferenceIdentifier = ""
+          end
+          sub_loop_child.DTP do |seg|
+            seg.DateTimeQualifier = ''
+            seg.DateTimePeriodFormatQualifier = ''
+            seg.DateTimePeriod = ''
+          end
+        end
+      end
+    end
+    ts.SE do |seg|
+      seg.NumberOfIncludedSegments = ''
+      seg.TransactionSetControlNumber = ''
     end
     puts ts.to_s 
   end #end self.run
