193e0260c5baee9b8df09112ad7c1e048f9607a2
Robert Jackson <robertj@promedicalinc.com>
2011-09-06 12:09:58 -0400

    Update segments and transaction_sets autoloads.

--- lib/hippo/transaction_sets.rb
+++ lib/hippo/transaction_sets.rb
@@ -1,7 +1,11 @@
-module Hippo::TransactionSets
-    autoload :HIPAA_835,      'hippo/transaction_sets/HIPAA_835'
-    autoload :HIPAA_837,      'hippo/transaction_sets/HIPAA_837'
-    autoload :HIPAA_276,      'hippo/transaction_sets/HIPAA_276'
-    autoload :HIPAA_270,      'hippo/transaction_sets/HIPAA_270'
-    autoload :Base,           'hippo/transaction_sets/base'
+module Hippo
+  module TransactionSets
+
+    autoload :Base,      'hippo/transaction_sets/base'
+    autoload :HIPAA_276,  'hippo/transaction_sets/HIPAA_276'
+    autoload :HIPAA_277,  'hippo/transaction_sets/HIPAA_277'
+    autoload :HIPAA_835,  'hippo/transaction_sets/HIPAA_835'
+    autoload :HIPAA_837,  'hippo/transaction_sets/HIPAA_837'
+    autoload :HIPAA_999,  'hippo/transaction_sets/HIPAA_999'
+  end
 end
